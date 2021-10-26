90539e458626beb94b74d80fcfb682bdefe7aaa4
FranFranJams <SuperNewComputer16@outlook.com>
2021-10-26 17:42:51 +0100

    Create Pong.py

---
+++ Pong.py
@@ -0,0 +1,8 @@
+# Easy Pong Game in Python3 for Beginners
+# By Fractured (Manager: Fran)
+# Requirements: Have Python Installed (I am using Python 3.9.6) - link in desc
+
+# Part 1: Setting up the window and Game Loop
+# All episodes will be found on my Github - link in desc
+
+
